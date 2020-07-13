253426edf1349660c1abfbb5c1692d77c0294491
Ellery Aguilar <ellery.aguilar@avantica.net>
2020-07-12 22:21:54 -0600

    correct class name

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-    <section class="container wrapper about-us">
+    <section class="container wrapper about">
         <h2>Comercializadora Ginedia</h2>
         <img src="{{ asset('storage/images/acerca-de.jpg') }}" />
         <p>Nos dedicamos a la  importación, distribución y comercialización de puertas automáticas, herrajes y accesorios para la industria del vidrio arquitectónico de interiores y exteriores, fachadas de edificios, construcciones comerciales y residenciales.<br><br>
